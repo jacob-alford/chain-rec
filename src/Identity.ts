7898b95efbc3e9ececf1aec907a55787a6d53f36
Jacob Alford <jacob.alford@me.com>
2023-09-09 17:26:48 -0600

    feat: add identity re-exports

---
+++ src/Identity.ts
@@ -0,0 +1,9 @@
+import { ChainRec } from 'fp-ts/Identity'
+
+/**
+ * A re-export of fp-ts > Identity.chainRec
+ *
+ * @since 1.0.0
+ * @category Model
+ */
+export const chainRec = ChainRec.chainRec
