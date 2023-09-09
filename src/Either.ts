ea7abe6534a340afa9bd60bb5008470876bba582
Jacob Alford <jacob.alford@me.com>
2023-09-09 16:39:22 -0600

    chore: add chainRec re-exports

---
+++ src/Either.ts
@@ -0,0 +1,9 @@
+import { ChainRec } from 'fp-ts/Either'
+
+/**
+ * A re-export of fp-ts > Either.chainRec
+ *
+ * @since 1.0.0
+ * @category Model
+ */
+export const chainRec = ChainRec.chainRec
---
+++ src/IO.ts
@@ -0,0 +1,9 @@
+import { ChainRec } from 'fp-ts/IO'
+
+/**
+ * A re-export of fp-ts > IO.chainRec
+ *
+ * @since 1.0.0
+ * @category Model
+ */
+export const chainRec = ChainRec.chainRec
